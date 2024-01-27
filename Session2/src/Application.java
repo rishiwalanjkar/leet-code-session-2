7349f0ab758fbe10ba166e07277f57dae8e867f2
rishiwalanjkar <rishi.walanjkar@gmaile.com>
2024-01-27 21:17:50 +0530

    Search Insert, Plus One, Merge Sorted Array

--- Session2/src/Application.java
+++ Session2/src/Application.java
@@ -10,7 +10,15 @@ public class Application {
 		// TODO Auto-generated method stub
 		
 		try(Scanner userInputScanner = new Scanner(System.in);) {
-			List<String> programList = Arrays.asList("Two Sum", "Remove Duplicates", "Remove Element", "Exit");
+			List<String> programList = Arrays.asList(
+					"Two Sum", 
+					"Remove Duplicates", 
+					"Remove Element", 
+					"Search Insert", 
+					"Plus One", 
+					"Merge Sorted Array",
+					"Exit"
+					);
 			String userInput;
 			
 			PROGRAM_ITERATOR : do {
@@ -24,8 +32,9 @@ public class Application {
 				
 				try {
 	
-					int[] nums;
-					
+					int[] nums, nums1, nums2;
+					int m, n;
+
 					switch(Integer.parseInt(userInput)) {
 						case 1:
 							System.out.println("################### Sample 1 ###################");
@@ -78,8 +87,102 @@ public class Application {
 							System.out.println("Actual Output: " + removeElement(nums, 2) + ", " + Arrays.toString(nums));
 							System.out.println();
 							break;
-	
+							
 						case 4:
+							nums = new int[]{1, 3, 5, 6};
+
+							System.out.println("################### Sample 1 ###################");
+							System.out.println("Input: nums = [1,3,5,6], target = 5");
+							System.out.println("Expected Output: 2");
+							System.out.println("Actual Output: " + searchInsert(nums, 5));
+							System.out.println();
+
+							System.out.println("################### Sample 2 ###################");
+							System.out.println("Input: nums = [1,3,5,6], target = 2");
+							System.out.println("Expected Output: 1");
+							System.out.println("Actual Output: " + searchInsert(nums, 2));
+							System.out.println();
+
+							System.out.println("################### Sample 3 ###################");
+							System.out.println("Input: nums = [1,3,5,6], target = 7");
+							System.out.println("Expected Output: 4");
+							System.out.println("Actual Output: " + searchInsert(nums, 7));
+							System.out.println();
+							
+							nums = new int[]{1};
+
+							System.out.println("################### Sample 4 ###################");
+							System.out.println("Input: nums = [1], target = 1");
+							System.out.println("Expected Output: 0");
+							System.out.println("Actual Output: " + searchInsert(nums, 0));
+							System.out.println();
+							break;
+							
+						case 5:
+							nums = new int[]{1, 2, 3};
+
+							System.out.println("################### Sample 1 ###################");
+							System.out.println("Input: digits = [1,2,3]");
+							System.out.println("Expected Output: [1,2,4]");
+							System.out.println("Actual Output: " + Arrays.toString(plusOne(nums)));
+							System.out.println();
+
+							nums = new int[]{4, 3, 2, 1};
+
+							System.out.println("################### Sample 2 ###################");
+							System.out.println("Input: digits = [4,3,2,1]");
+							System.out.println("Expected Output: [4,3,2,2]");
+							System.out.println("Actual Output: " + Arrays.toString(plusOne(nums)));
+							System.out.println();
+
+							nums = new int[]{9};
+
+							System.out.println("################### Sample 3 ###################");
+							System.out.println("Input: digits = [9]");
+							System.out.println("Expected Output: [1,0]");
+							System.out.println("Actual Output: " + Arrays.toString(plusOne(nums)));
+							System.out.println();
+							break;
+							
+						case 6:
+							nums1	= new int[] {1,2,3,0,0,0};
+							nums2	= new int[] {2,5,6};
+							m		= 3;
+							n		= 3;
+
+							System.out.println("################### Sample 1 ###################");
+							System.out.println("Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3");
+							System.out.println("Expected Output: [1,2,2,3,5,6]");
+							mergeSortedArray(nums1, m, nums2, n);
+							System.out.println("Actual Output: " + Arrays.toString(nums1));
+							System.out.println();
+
+							nums1	= new int[] {1};
+							nums2	= new int[] {};
+							m		= 1;
+							n		= 0;
+
+							System.out.println("################### Sample 2 ###################");
+							System.out.println("Input: nums1 = [1], m = 1, nums2 = [], n = 0");
+							System.out.println("Expected Output: [1]");
+							mergeSortedArray(nums1, m, nums2, n);
+							System.out.println("Actual Output: " + Arrays.toString(nums1));
+							System.out.println();
+
+							nums1	= new int[] {0};
+							nums2	= new int[] {1};
+							m		= 0;
+							n		= 1;
+
+							System.out.println("################### Sample 3 ###################");
+							System.out.println("Input: nums1 = [0], m = 0, nums2 = [1], n = 1");
+							System.out.println("Expected Output: [1]");
+							mergeSortedArray(nums1, m, nums2, n);
+							System.out.println("Actual Output: " + Arrays.toString(nums1));
+							System.out.println();
+							break;
+	
+						case 7:
 							break PROGRAM_ITERATOR;
 	
 						default:
@@ -130,6 +233,65 @@ public class Application {
 			if(val != nums[i])
 				nums[j++] = nums[i];
 		
-		return j++;
+		return j;
+    }
+	
+	 private static int searchInsert(int[] nums, int target) {
+		 int start = 0,
+			end = nums.length - 1,
+			mid = 0 == end ? 0 : end / 2,
+			indexOfTarget = -1;
+		 
+		 while(start < end) {
+			 if(target == nums[mid]) {
+				 indexOfTarget = mid;				 
+				 break;
+			 } else if(target < nums[mid])
+				 end = mid;
+			 else
+				 start = mid + 1;
+
+			 mid = start + (end - start)/2;
+		 }
+		 
+		 if(-1 < indexOfTarget) return indexOfTarget;
+		 
+		 indexOfTarget = target == nums[start] || target < nums[start] ? start : start + 1;
+		 
+		 return indexOfTarget;
+	}
+	 
+	private static int[] plusOne(int[] digits) {
+		
+		DIGITS_ITERATOR : for(int i = digits.length - 1; i >= 0; i--) {
+			switch(digits[i] + 1) {
+				case 10:
+					digits[i] = 0;
+					break;
+				
+				default:
+					digits[i] += 1;
+					break DIGITS_ITERATOR;
+			}
+		}
+		
+		if(0 != digits[0]) return digits;
+		
+		int[] result = new int[digits.length + 1];
+		
+		for(int i = result.length - 1; i > 0; i--)
+			result[i] = digits[i - 1];
+		
+		result[0] = 1;
+		
+		return result;
+    }
+	
+	private static void mergeSortedArray(int[] nums1, int m, int[] nums2, int n) {
+        for(int i = m - 1, j = n - 1, k = nums1.length - 1; k >= 0 ; k--)
+        	if(0 <= i && 0 <= j)
+        		nums1[k] = nums1[i] > nums2[j] ? nums1[i--] : nums2[j--];
+        	else if(0 > i)
+        		nums1[k] = nums2[j--];
     }
 }
